3c4a69cb39eed6347f424f8790a2b262a9939ca6
Irina <kalashiri-sweet@mail.ru>
2023-02-07 20:17:39 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {System.out.println("Приложение, выводящее итоговый счёт и количество бонусных рублей");
+        int score = 100;// начальные данные
+        int deposit = 1100; //сумма пополнения счета
+
+        int bonus;
+
+        if (deposit > 1000) {
+            bonus = deposit / 100;//формула подсчета бонусных рублей за каждые доп внесенные 100р на счет.
+        } else {
+            bonus = 0;
+        }
+        int balance = score + deposit + bonus;
+        System.out.println("Ваш итоговый счет: " + balance + " руб.");
+        System.out.println("Ваш бонус: " + bonus + " руб.");
+
     }
 }
